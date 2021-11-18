a1e25f86e4f235c3672305854153b98e7205801d
Maria Antônia Amado <48838678+AmadoMaria@users.noreply.github.com>
2021-11-17 23:34:11 -0300

    Update FamilyAncestral.cs

--- FamilyComposite/FamilyAncestral.cs
+++ FamilyComposite/FamilyAncestral.cs
@@ -6,17 +6,43 @@ using System.Text;
 namespace FamilyComposite
 {
     public class FamilyAncestral : FamilyComponent
-    {
-        
+    {        
         public List<FamilyComponent> familyComponents = new List<FamilyComponent>();
         public string Name { get; set; }
         public IEnumerator Iterator { get; set; } = null;
 
-        public override IEnumerator CreateIterator() {
+        public FamilyAncestral(string name){
+            this.Name = name;
+        }
+
+        public override void Add(FamilyComponent familyComponent) {
+            familyComponents.Add(familyComponent);
+        }
+
+        public override void Remove(FamilyComponent familyComponent) {
+            familyComponents.Remove(familyComponent);
+        }
+
+        public override string GetName() {
+            return this.Name;
+        }
+
+        public override void Print() {
+            Console.WriteLine("\n" + GetName());        
+            Console.WriteLine("----------------------");
+
+            IEnumerator Iterator = familyComponents.GetEnumerator();
+            while (Iterator.MoveNext()) {
+                FamilyComponent familyComponent = (FamilyComponent)Iterator.Current;
+                familyComponent.Print();
+            }
+        }
+
+        public override CompositeIterator CreateIterator() {
             if (Iterator == null) {
                 Iterator = new CompositeIterator(familyComponents.GetEnumerator());
             }
-            return Iterator;
+            return (CompositeIterator)Iterator;
         }
 
     }
