84313fa343375de253e65e8a79db61c76ecc80e4
Maria Antônia Amado <48838678+AmadoMaria@users.noreply.github.com>
2021-11-17 23:33:52 -0300

    Update Program.cs

--- FamilyComposite/Program.cs
+++ FamilyComposite/Program.cs
@@ -6,7 +6,33 @@ namespace FamilyComposite
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            FamilyAncestral maria = new FamilyAncestral("Bisavó Maria");
+            FamilyAncestral joana = new FamilyAncestral("Avô Joana");
+            FamilyAncestral luisa = new FamilyAncestral("Avó Luisa");
+            FamilyAncestral sandra = new FamilyAncestral("Mãe Sandra");
+
+            FamilyDescendant carla = new FamilyDescendant("Carla", true);
+            FamilyDescendant bia = new FamilyDescendant("Bia", false);
+            FamilyDescendant diana = new FamilyDescendant("Diana",false);
+            FamilyDescendant malu = new FamilyDescendant("Malu", true);
+            FamilyDescendant sofia = new FamilyDescendant("Sofia", true);
+
+            maria.Add(joana);
+            maria.Add(luisa);
+
+            joana.Add(carla);
+            joana.Add(bia);
+
+            luisa.Add(diana);
+            luisa.Add(sandra);
+
+            sandra.Add(malu);
+            sandra.Add(sofia);
+
+            Client client = new Client(maria);
+
+            client.PrintLeafArtistComponents();
+
         }
     }
 }
